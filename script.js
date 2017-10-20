acecd3d61058312949460971460d807b97d461cf
reshnaz <31286438+reshnaz@users.noreply.github.com>
2017-10-20 16:58:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// This is the jQuery function that will display help text on hovering over the form fields.
+ $(function() {
+    $("#name").tooltip();
+    $("#email").tooltip();
+    $("#pwd").tooltip();
+    $("#cpwd").tooltip();
+ });
